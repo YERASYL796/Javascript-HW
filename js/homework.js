30b1d692b1eb03ebee021b07ec3700a5c7af4cda
erasyl <agabekerasyl2@gmail.com>
2024-10-31 16:15:05 +0500

    added homework files

---
+++ js/homework.js
@@ -0,0 +1,37 @@
+var name= 'YERA'
+
+var azamat;
+
+console.log(" YERASILDYN mani: " + azamat);
+
+console.log(typeof name); //string
+
+var age= 14  
+
+console.log("age mani: " + 14);
+
+console.log(typeof age); //number
+
+ var age= toString("15")
+
+ 
+console.log(typeof name); //string
+
+
+var durys = true;
+var qate = false;
+
+var san1 = 10
+var san2 = 15
+
+natizhe = san1 + san2 // 10 + 15
+console.log("Ерасылдын нәтижесі:  " + natizhe);
+
+
+if(10 + 15 == 25){
+    console.log(durys);
+}
+else{
+    console.log(qate);    
+}
+console.log(typeof Boolean); //Buoleon
